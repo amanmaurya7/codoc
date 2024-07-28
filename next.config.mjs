cfea854be547503139e29931f44a703a82dd2b81
Aman Maurya <amanmaurya.me@gmail.com>
2024-07-28 09:56:44 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
-  },
-};
