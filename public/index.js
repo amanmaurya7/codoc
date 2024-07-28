756eee0c82ed27d7a8ab8d0ae821c4d24853e7dc
Ritesh jha <jhar27018@gmail.com>
2024-07-28 15:14:34 +0530

    some static assets were remainng in previous commit

---
+++ public/index.js
@@ -0,0 +1,67 @@
+import play from "./play.svg";
+import gradient from "./gradient.png";
+import smallSphere from "./4-small.png";
+import grid from "./grid.png";
+import loading1 from "./loading-01.svg";
+import yourlogo from "./yourlogo.svg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+import disc02 from "./disc-02.svg";
+import chromecast from "./chrome-cast.svg";
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+import background from "./background.jpg";
+
+import curve from "./assets/hero/curve.png";
+import robot from "./assets/hero/robot.jpg";
+import heroBackground from "./assets/hero/hero-background.jpg";
+
+import benefitCard1 from "./assets/benefits/card-1.svg";
+import benefitCard2 from "./assets/benefits/card-2.svg";
+import benefitCard3 from "./assets/benefits/card-3.svg";
+import benefitCard4 from "./assets/benefits/card-4.svg";
+import benefitCard5 from "./assets/benefits/card-5.svg";
+import benefitCard6 from "./assets/benefits/card-6.svg";
+import benefitIcon1 from "./assets/benefits/icon-1.svg";
+import benefitIcon2 from "./assets/benefits/icon-2.svg";
+import benefitIcon3 from "./assets/benefits/icon-3.svg";
+import benefitIcon4 from "./assets/benefits/icon-4.svg";
+import benefitImage2 from "./assets/benefits/image-2.png";
+
+export {
+  loading1,
+  play,
+  gradient,
+  smallSphere,
+  grid,
+  yourlogo,
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+  loading,
+  background,
+  curve,
+  robot,
+  heroBackground,
+  benefitCard1,
+  benefitCard2,
+  benefitCard3,
+  benefitCard4,
+  benefitCard5,
+  benefitCard6,
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import ButtonGradient from "../components/ui/svg/ButtonGradient";
+import Benefits from "../components/Benefits";
+import Hero from "../components/Hero";
+import Header from "../components/HeaderHero";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
