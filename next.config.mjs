6f210e25bcb7148802cc098569499facb6bfa2a6
Ritesh jha <jhar27018@gmail.com>
2024-07-28 10:02:16 +0530

    reinitialized the repository

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
+  },
+};
---
+++ src/app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,46 @@
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { getClerkUsers } from "../../../../lib/actions/user.actions";
+import { getDocument } from "../../../../lib/actions/room.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Document = async ({ params: { id } }) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({ userIds });
+
+  const usersData = users.map((user) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes("room:write")
+      ? "editor"
+      : "viewer",
+  }));
+
+  const currentUserType = room.usersAccesses[
+    clerkUser.emailAddresses[0].emailAddress
+  ]?.includes("room:write")
+    ? "editor"
+    : "viewer";
+
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        users={usersData}
+        currentUserType={currentUserType}
+      />
+    </main>
+  );
+};
+
+export default Document;
